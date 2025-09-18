b4d427839de3e30409265ce084084abdda3080de
yefu <243249439@qq.com>
2025-09-19 00:48:21 +0800

    feat: 添加Starlight集成并更新相关依赖

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
+import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), solidJs()],
+  integrations: [
+    starlight({
+      customCss: ["./src/styles/office.css"],
+      title: "夜浮卿的个人博客和分享",
+    }),
+    // mdx(),
+    sitemap(),
+    solidJs(),
+  ],
   server: {
     allowedHosts: ["yefu24324.com", "host.docker.internal"],
     host: true,
